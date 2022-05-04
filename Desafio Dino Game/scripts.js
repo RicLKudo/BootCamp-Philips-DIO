034ed84aae8dea0604f4b41600fb9849c5c688fe
RicLKudo <ricardo.kudo@outlook.com>
2022-05-04 02:11:10 -0300

    atualização dos arquivos do projeto

--- Desafio Dino Game/scripts.js
+++ Desafio Dino Game/scripts.js
@@ -1,14 +1,31 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
 
-function handleKeyUp(event){
+function handleKeyDown(event){
     if (event.keyCode === 38) {
+        if (!isJumping) {
         jump();
+        pontua();
         console.log('pressionou seta para cima!');
+        }
+    }
+}
+
+function pontua(){
+    let score = 0;
+    if(position >= 60){
+        score += 10;
+        console.log(score);
     }
 }
 
 function jump(){
-    let position = 0;
+   
+    isJumping = true;
+
     let upInterval = setInterval(()=> {
         if (position >= 150){
             clearInterval(upInterval)
@@ -16,6 +33,7 @@ function jump(){
     let downInterval = setInterval(() => {
         if (position <= 0){
             clearInterval(downInterval);
+            isJumping = false;
         }
         else{
             position -=20;
@@ -28,4 +46,37 @@ function jump(){
         }
     }, 20);
 }
-document.addEventListener('keyup', handleKeyUp);
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    if(isGameOver) return;
+
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftInterval);
+            isGameOver = true;
+            document.body.innerHTML = '<h1 class="gameover">Fim de jogo</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+
+        }
+    },20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keydown', handleKeyDown);
